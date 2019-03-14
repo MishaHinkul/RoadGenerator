266cff987a46a258f8229846d47441fc9ed8eb5b
MichaelHinkul <mykhailohinkuljob@gmail.com>
2019-03-14 21:28:21 +0200

    [*] Refactoring EventGlobal

--- Assets/Scripts/EventGlobal.cs
+++ Assets/Scripts/EventGlobal.cs
@@ -3,29 +3,29 @@ using System.Collections;
 
 public enum EventGlobal
 {
-    None,
-    E_AppStart,
-    E_AppUpdate,
-    E_AppFixedUpdate,
-    E_AppLateUpdate,
+  None,
+  E_AppStart,
+  E_AppUpdate,
+  E_AppFixedUpdate,
+  E_AppLateUpdate,
 
-    E_WaitTime,
+  E_WaitTime,
 
-    //Roads
-    E_GeneradeRoads,
-    E_SplitSegmentForLevel,
-    E_SplitSegment,
-    E_SetTemplate,
+  //Roads
+  E_GeneradeRoads,
+  E_SplitSegmentForLevel,
+  E_SplitSegment,
+  E_SetTemplate,
 
-    //Car
-    E_CarLogics,
-    E_LeaveСity,
+  //Car
+  E_CarLogics,
+  E_LeaveСity,
 
-    //Camera
-    E_CameraMove,
-    E_CameraScale,
-    E_CameraUpdateSettings,
+  //Camera
+  E_CameraMove,
+  E_CameraScale,
+  E_CameraUpdateSettings,
 
-    //Debug
-    E_Dubug_ShowPath
+  //Debug
+  E_Dubug_ShowPath
 }
