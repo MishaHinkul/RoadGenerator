4daa205506c6c747b8ba74808b0d392e510ce6fd
MichaelHinkul <mykhailohinkuljob@gmail.com>
2019-03-14 21:28:02 +0200

    [*] Refactoring CoroutineExecutor

--- Assets/Scripts/CoroutineExecutor.cs
+++ Assets/Scripts/CoroutineExecutor.cs
@@ -5,53 +5,58 @@ using strange.extensions.context.api;
 
 namespace strange.examples.strangerocks
 {
-    [Implements(typeof(ICoroutineExecutor), InjectionBindingScope.CROSS_CONTEXT)]
-    public class CoroutineExecutor : ICoroutineExecutor
+  [Implements(typeof(ICoroutineExecutor), InjectionBindingScope.CROSS_CONTEXT)]
+  public class CoroutineExecutor : ICoroutineExecutor
+  {
+    private CoroutineMonoBehaviour mb = null;
+
+    [PostConstruct]
+    public void PostConstruct()
     {
-
-        [Inject(ContextKeys.CONTEXT_VIEW)]
-        public GameObject contextView { get; set; }
-
-        private CoroutineMonoBehaviour mb;
-
-        [PostConstruct]
-        public void PostConstruct()
-        {
-            mb = contextView.AddComponent<CoroutineMonoBehaviour>();
-        }
-
-        public Coroutine StartCroutine(string methodName)
-        {
-            return mb.StartCoroutine(methodName);
-        }
-        public Coroutine StartCoroutine(IEnumerator method)
-        {
-            return mb.StartCoroutine(method);
-        }
-        public Coroutine StartCoroutine(string methodName, object value)
-        {
-            return mb.StartCoroutine(methodName, value);
-        }
-
-        public void StopCoroutine(string methodName)
-        {
-            mb.StopCoroutine(methodName);
-        }
-        public void StopCoroutine(IEnumerator method)
-        {
-            mb.StopCoroutine(method);
-        }
-        public void StopCoroutine(Coroutine method)
-        {
-            mb.StopCoroutine(method);
-        }
-        public void StopAllCoroutine()
-        {
-            mb.StopAllCoroutines();
-        }
+      mb = contextView.AddComponent<CoroutineMonoBehaviour>();
     }
 
-    public class CoroutineMonoBehaviour : MonoBehaviour
+    public Coroutine StartCroutine(string methodName)
     {
+      return mb.StartCoroutine(methodName);
     }
+
+    public Coroutine StartCoroutine(IEnumerator method)
+    {
+      return mb.StartCoroutine(method);
+    }
+
+    public Coroutine StartCoroutine(string methodName, object value)
+    {
+      return mb.StartCoroutine(methodName, value);
+    }
+
+    public void StopCoroutine(string methodName)
+    {
+      mb.StopCoroutine(methodName);
+    }
+
+    public void StopCoroutine(IEnumerator method)
+    {
+      mb.StopCoroutine(method);
+    }
+
+    public void StopCoroutine(Coroutine method)
+    {
+      mb.StopCoroutine(method);
+    }
+
+    public void StopAllCoroutine()
+    {
+      mb.StopAllCoroutines();
+    }
+
+
+    [Inject(ContextKeys.CONTEXT_VIEW)]
+    public GameObject contextView { get; set; }
+  }
+
+  public class CoroutineMonoBehaviour : MonoBehaviour
+  {
+  }
 }
