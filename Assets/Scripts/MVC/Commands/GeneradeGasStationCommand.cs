29b70d4b6c88687e3d77490e28128e70a734af57
MichaelHinkul <mykhailohinkuljob@gmail.com>
2019-03-17 23:03:55 +0200

    [*] Refactoring GeneradeGasStationCommand

--- Assets/Scripts/MVC/Commands/GeneradeGasStationCommand.cs
+++ Assets/Scripts/MVC/Commands/GeneradeGasStationCommand.cs
@@ -4,65 +4,77 @@ using UnityEngine;
 
 public class GeneradeGasStationCommand : BaseCommand
 {
-    [Inject]
-    public RoadNetworkModel networkModel { get; private set; }
+  private const int GAS_STATION_COUNT = 3;
 
-    [Inject]
-    public PopulationsModel populationModel { get; private set; }
+  private List<RoadSegment> gasSegment = new List<RoadSegment>();
 
-    public override void Execute()
+  public override void Execute()
+  {
+    RoadSegment segment = null;
+    GameObject build = null;
+
+    for (int i = 0; i < GAS_STATION_COUNT; i++)
     {
-        int gasStationCpunt = 3;
-        List<RoadSegment> gasSegment = new List<RoadSegment>();
-
-        //Определяем случайные сегменты относительно которых будет создаваться заправка
-        for (int i = 0; i < gasStationCpunt; i++)
-        {
-            bool addEntry = false;
-            do
-            {
-                int randomIndex = Random.Range(0, networkModel.RoadSegments.Count);
-                RoadSegment segment = networkModel.RoadSegments[randomIndex];
-                if (!gasSegment.Contains(segment))
-                {
-                    gasSegment.Add(segment);
-                    addEntry = true;
-                }
-            }
-            while (!addEntry);
-        }
-
-        //Создаем заправочные станции
-        
-        for (int i = 0; i < gasSegment.Count; i++)
-        {
-            Vector2 start = gasSegment[i].Begin.Point;
-            Vector2 end = gasSegment[i].End.Point;
-            Vector2 dir = (end - start).normalized;
-            float distance = Vector2.Distance(start, end);
-
-            Vector2 current = start;
-
-            float level = 2.0f - (gasSegment[i].Level / 3f);//0,0.33,0.66,1
-            float length = Random.Range(1.5f, 2f) * level;
-            float height = Random.Range(3f, 6f) * level;
-
-            Vector2 per = new Vector2(-dir.y, dir.x);
-            Vector2 roadOffset = per.normalized * (networkModel.WithRoad * 1.5f + length);
-            float factor = distance * 0.5f;
-            float wight = distance * 0.3f;
-
-            Vector2 tc = start + (dir * factor) + roadOffset;
-            Vector3 center = new Vector3(tc.x, 0, tc.y);
-            GameObject build = GameObject.CreatePrimitive(PrimitiveType.Cube);
-            if (build != null)
-            {
-                build.transform.position = center;
-                build.name = "Gass Station " + i;
-                build.transform.localScale = new Vector3(factor, height, wight);
-
-                populationModel.Buildings.Add(build);
-            }       
-        }
+      segment = SetGassStationSegment();
+      build = BuildGassStation(segment);
+      PopulationModel.Buildings.Add(build);
     }
+  }
+
+  private RoadSegment SetGassStationSegment()
+  {
+    int randomIndex = 0;
+    bool addEntry = false;
+    RoadSegment segment = null;
+    do
+    {
+      randomIndex = Random.Range(0, NetworkModel.RoadSegments.Count);
+      segment = NetworkModel.RoadSegments[randomIndex];
+      if (!gasSegment.Contains(segment))
+      {
+        gasSegment.Add(segment);
+        addEntry = true;
+      }
+    }
+    while (!addEntry);
+
+    return segment;
+  }
+
+  private GameObject BuildGassStation(RoadSegment segment)
+  {
+    Vector2 start = segment.Begin.Point;
+    Vector2 end = segment.End.Point;
+    Vector2 dir = (end - start).normalized;
+    float distance = Vector2.Distance(start, end);
+
+    Vector2 current = start;
+
+    float level = 2.0f - (segment.Level / 3f);//0,0.33,0.66,1
+    float length = Random.Range(1.5f, 2f) * level;
+    float height = Random.Range(3f, 6f) * level;
+
+    Vector2 per = new Vector2(-dir.y, dir.x);
+    Vector2 roadOffset = per.normalized * (NetworkModel.WithRoad * 1.5f + length);
+    float factor = distance * 0.5f;
+    float wight = distance * 0.3f;
+
+    Vector2 tc = start + (dir * factor) + roadOffset;
+    Vector3 center = new Vector3(tc.x, 0, tc.y);
+
+
+    GameObject build = GameObject.CreatePrimitive(PrimitiveType.Cube);
+    build.transform.position = center;
+    build.name = "Gass Station";
+    build.transform.localScale = new Vector3(factor, height, wight);
+
+    return build;
+  }
+
+
+  [Inject]
+  public RoadNetworkModel NetworkModel { get; private set; }
+
+  [Inject]
+  public PopulationsModel PopulationModel { get; private set; }
 }
